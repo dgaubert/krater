49ce6e200b15d5e999958c482ac0d4bb0f5a7ae6
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-07-15 11:57:53 +0200

    Do not expose CONNECT method

--- src/methods.js
+++ src/methods.js
@@ -3,7 +3,9 @@ import { METHODS } from 'http'
 export default METHODS.reduce((methods, method) => {
   if (method === 'DELETE') {
     methods[method] = 'del'
-  } else {
+  } else if (method !== 'CONNECT') {
+    // proxy-chaining and tunneling out of scope
+    // see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/CONNECT
     methods[method] = method.toLowerCase()
   }
   return methods
