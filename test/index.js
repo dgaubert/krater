da3bfac5c91b63d8854d3f2e9ec9ea0b9f4c26e9
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-07-15 11:58:16 +0200

    Fix test to cover all http methods

--- test/index.js
+++ test/index.js
@@ -4,26 +4,27 @@ import { default as Router } from '../src'
 import methods from '../src/methods'
 import Koa from 'koa'
 
-class User extends Router {
-  get () {
-    return ctx => {
-      ctx.body = ctx.params[0]
-    }
-  }
-}
-
 const USER = 'foo'
 
-Object.keys(methods).forEach(function (key) {
-  const method = methods[key]
+Object.keys(methods).forEach(function (method) {
+  const controllerMethod = methods[method]
 
-  describe('route.' + method + '()', function () {
+  class User extends Router {
+    [ controllerMethod ] () {
+      return ctx => {
+        ctx.body = ctx.params[0]
+      }
+    }
+  }
+
+  describe('route.' + controllerMethod + '()', function () {
     before(function (done) {
       const app = new Koa()
       const user = new User()
+
       user.path(`/:user(${USER})`).regist(app)
 
-      this.httpServer = app.listen(3000)
+      this.httpServer = app.listen()
 
       this.httpServer.once('listening', () => {
         this.port = this.httpServer.address().port
@@ -37,21 +38,24 @@ Object.keys(methods).forEach(function (key) {
 
     describe('when method and path match', function () {
       it('should 200', async function () {
-        const res = await fetch(`http://localhost:${this.port}/${USER}`)
+        const res = await fetch(`http://localhost:${this.port}/${USER}`, { method })
         const body = await res.text()
 
-        assert.ok(res.ok)
+        assert.ok(res.ok, res.statusText)
         assert.equal(res.status, 200)
         assert.equal(res.headers.get('content-type'), 'text/plain; charset=utf-8')
-        assert.equal(body, USER)
+
+        if (method !== 'HEAD') {
+          assert.equal(body, USER)
+        }
       })
     })
 
     describe('when only method matches', function () {
       it('should 404', async function () {
-        const res = await fetch(`http://localhost:${this.port}/bar`)
+        const res = await fetch(`http://localhost:${this.port}/bar`, { method })
 
-        assert.ok(!res.ok)
+        assert.ok(!res.ok, res.statusText)
         assert.equal(res.status, 404)
       })
     })
