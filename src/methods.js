fcaf2ba85c1bbd3488aeb22662e31b09db222215
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-07-16 09:58:57 +0200

    typo

--- src/methods.js
+++ src/methods.js
@@ -4,7 +4,7 @@ export default METHODS.reduce((methods, method) => {
   if (method === 'DELETE') {
     methods[method] = 'del'
   } else if (method !== 'CONNECT') {
-    // proxy-chaining and tunneling out of scope
+    // proxy-chaining and tunneling is out of scope
     // see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/CONNECT
     methods[method] = method.toLowerCase()
   }
